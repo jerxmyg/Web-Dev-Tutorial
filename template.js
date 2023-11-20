ffaaf30c0d032062337402341868349415204d6b
jer <32397605+jerxmyg@users.noreply.github.com>
2023-11-20 15:45:03 -0500

    Final commits

--- template.js
+++ template.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     // This example adds a click event to the "Introduction" div
-    var introductionDiv = document.querySelector('.meep');
+    var introductionDiv = document.querySelector('.Introduction');
 
     introductionDiv.addEventListener('click', function () {
-        alert('Hello! This is the Introduction section.');
+        alert('Hello! This is the Introduction section. not really sure how to implement js with such a basic template. you guys can figure that out!');
     });
 });
